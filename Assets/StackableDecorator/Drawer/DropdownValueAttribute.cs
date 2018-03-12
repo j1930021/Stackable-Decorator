f20d71cbe4e440967afca678e8c9cdfb4986b5dc
Kinwailo <lokinwai@gmail.com>
2018-03-12 04:48:44 -0500

    Update DropdownValue
    Add placeholder and static name list

--- Assets/StackableDecorator/Drawer/DropdownValueAttribute.cs
+++ Assets/StackableDecorator/Drawer/DropdownValueAttribute.cs
@@ -9,14 +9,19 @@ namespace StackableDecorator
     public class DropdownValueAttribute : StackableFieldAttribute
     {
         public string names = null;
+        public string placeHolder = string.Empty;
 #if UNITY_EDITOR
         private string m_ValuesGetter;
+        private string[] m_NameArray = null;
 
         private string[] m_Names = new string[0];
         private object[] m_Values = new object[0];
 
+        private static GUIContent m_Content = new GUIContent(" ");
+        private static GUIStyle m_Style = null;
+
         private DynamicValue<string[]> m_DynamicNames = new DynamicValue<string[]>();
-        private DynamicValue<object> m_DynamicValue = new DynamicValue<object>();
+        private DynamicValue<object> m_DynamicValues = new DynamicValue<object>();
 #endif
         public DropdownValueAttribute(string values)
         {
@@ -45,19 +50,29 @@ namespace StackableDecorator
                     return;
             }
 
-            int selected = 0;
+            if (m_Style == null)
+            {
+                m_Style = new GUIStyle(EditorStyles.popup);
+                m_Style.normal.background = null;
+            }
+
+            int selected = -1;
             var propertyValue = property.GetValueAsObject();
-            m_DynamicNames.UpdateAndCheckInitial(this.names, property);
-            m_DynamicValue.UpdateAndCheckInitial(m_ValuesGetter, property, m_FieldInfo.FieldType.MakeArrayType());
-            var values = (Array)m_DynamicValue.GetValue();
+            m_DynamicNames.UpdateAndCheckInitial(names, property);
+            m_DynamicValues.UpdateAndCheckInitial(m_ValuesGetter, property, m_FieldInfo.FieldType.MakeArrayType());
+            var values = (Array)m_DynamicValues.GetValue();
             if (values == null)
             {
                 EditorGUI.LabelField(position, label.text, "Getter not correct.");
                 return;
             }
 
-            var names = this.names == null ? null : m_DynamicNames.GetValue();
-            if (this.names != null && names != null)
+            if (m_NameArray == null && names != null)
+                m_NameArray = names.Split(',');
+            if (names != null && !m_DynamicNames.IsStatic())
+                m_NameArray = m_DynamicNames.GetValue();
+
+            if (names != null && m_NameArray != null)
             {
                 var length = Math.Min(names.Length, values.Length);
                 if (m_Names.Length != length)
@@ -66,7 +81,7 @@ namespace StackableDecorator
                     m_Values = new object[length];
                 for (int i = 0; i < length; i++)
                 {
-                    m_Names[i] = names[i];
+                    m_Names[i] = m_NameArray[i];
                     m_Values[i] = values.GetValue(i);
                     if (propertyValue.Equals(m_Values[i]))
                         selected = i;
@@ -90,7 +105,13 @@ namespace StackableDecorator
 
             label = EditorGUI.BeginProperty(position, label, property);
             selected = EditorGUI.Popup(position, label.text, selected, m_Names);
-            property.SetObjectToValue(m_Values[selected]);
+            if (selected < 0 || selected >= names.Length)
+            {
+                var rect = EditorGUI.PrefixLabel(position, m_Content);
+                GUI.Label(rect, placeHolder, m_Style);
+            }
+            else
+                property.SetObjectToValue(m_Values[selected]);
             EditorGUI.EndProperty();
         }
 #endif
